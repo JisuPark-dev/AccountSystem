21f4eaba6e77a8a4895994b8f33fed40e6c394df
jisupark-dev <dodlal123tkd@gmail.com>
2023-07-11 12:15:39 +0900

    [commit] account, accountUser 도메인 수정

--- src/main/java/com/zerobase/Account/controller/AccountController.java
+++ src/main/java/com/zerobase/Account/controller/AccountController.java
@@ -1,12 +1,13 @@
 package com.zerobase.Account.controller;
 
 import com.zerobase.Account.domain.Account;
+import com.zerobase.Account.dto.CreateAccount;
 import com.zerobase.Account.service.AccountService;
 import com.zerobase.Account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,16 +15,20 @@ public class AccountController {
     private final AccountService accountService;
     private final RedisTestService redisTestService;
 
+    @PostMapping("/account")
+    public CreateAccount.Response createAccount(
+            @RequestBody @Valid CreateAccount.Request request
+            ) {
+        accountService.createAccount();
+        return new CreateAccount.Response();
+    }
+
     @GetMapping("/get-lock")
     public String getLock() {
         return redisTestService.getLock();
     }
 
-    @GetMapping("/create-account")
-    public String createAccount() {
-        accountService.saveAccount();
-        return "ok";
-    }
+
 
     @GetMapping("account/{id}")
     public Account findAccount(@PathVariable Long id) {
--- src/main/java/com/zerobase/Account/service/AccountService.java
+++ src/main/java/com/zerobase/Account/service/AccountService.java
@@ -6,7 +6,6 @@ import com.zerobase.Account.repository.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.Enumerated;
 import javax.transaction.Transactional;
 
 @Service
@@ -15,12 +14,8 @@ public class AccountService {
     private final AccountRepository accountRepository;
 
     @Transactional
-    public void saveAccount() {
-        Account account = Account.builder()
-                .accountNumber("4000")
-                .accountStatus(AccountStatus.IN_USE)
-                .build();
-        accountRepository.save(account);
+    public void createAccount(Long userId, Long initialBalance) {
+
     }
 
     @Transactional
